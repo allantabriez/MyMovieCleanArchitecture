48460c5a29ca2f8c22246654959703f1b9f8ff06
allantabriez <allantabriez98@mail.ugm.ac.id>
2020-09-23 18:05:47 +0700

    added obfuscation and certificate pinning

--- core/src/main/java/com/example/madesubmission/core/di/CoreModule.kt
+++ core/src/main/java/com/example/madesubmission/core/di/CoreModule.kt
@@ -10,10 +10,14 @@ import com.example.madesubmission.core.domain.repository.IMovieRepository
 import com.example.madesubmission.core.ui.RecyclerViewAdapter
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 val databaseModule = module {
@@ -34,9 +38,24 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/HkCBucsA3Tgiby96X7vjb/ojHaE1BrjvZ2+LRdJJd0E=")
+            .add(hostname, "sha256/nKWcsYrc+y5I8vLf1VGByjbt+Hnasjl+9h8lNKJytoE=")
+            .add("sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=")
+            .build()
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
+    }
+    single {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.themoviedb.org/3/movie/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
             .build()
         retrofit.create(ApiService::class.java)
     }
